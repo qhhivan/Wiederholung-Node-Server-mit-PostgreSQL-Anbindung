e891e672efc6f8f952338e350f53155e7e5ab510
qhhivan <orsolic.i02@htlwienwest.at>
2021-10-19 14:38:23 +0200

    Model fertig

--- Server Angabe/model/cars.js
+++ Server Angabe/model/cars.js
@@ -32,6 +32,11 @@ async function changeStatus(id, status) {
 
     //   Update Befehl
     await db.query('UPDATE cars set status = $1 where id = $2', [status, id]);
+
+    return {
+      code: 200,
+      data: 'Status Changed',
+    };
   } catch (error) {
     return { code: 500, data: error.message };
   }
@@ -59,3 +64,60 @@ async function deleteCar(id) {
 }
 
 // d. Route zum Einfügen eines neuen Autos. Daten:
+async function addCar(car) {
+  try {
+    // max(ID) von cars holen
+    const { maxId } = await db.query('SELECT max(id) FROM cars');
+    const newCarId = maxId + 1;
+
+    //   Überprüfen ob es den Besitzer schon gibt
+    let { owner } = await db.query(
+      'SELECT * FROM owner WHERE first_name = $1 and last_name = $2',
+      [car.owner.firstName, car.owner.lastName],
+    );
+
+    if (owner.id === null) {
+      // Owner wird eingefügt
+      await db.query(
+        'INSERT INTO public.owner ( first_name, last_name) VALUES ($1, $2);',
+        [car.owner.firstName, car.owner.lastName],
+      );
+
+      // Owner wird als owner abgespeichert
+      owner = await db.query(
+        'SELECT * FROM owner WHERE first_name = $1 and last_name = $2',
+        [car.owner.firstName, car.owner.lastName],
+      );
+    }
+    // Owner ID holen
+    const ownerID = owner.id;
+
+    // Car erstellen
+    await db.query(
+      'insert into cars (id, title, image, status, price, miles, year_of_make, description, owner) values($1, $2, $3, $4, $5, $6, $7, $8, $9)',
+      [
+        newCarId,
+        car.title,
+        car.image,
+        car.status,
+        car.price,
+        car.miles,
+        car.yearOfMake,
+        car.description,
+        ownerID,
+      ],
+    );
+
+    return {
+      code: 200,
+      data: `Car added with ID ${newCarId}`,
+    };
+  } catch (error) {
+    return {
+      code: 500,
+      data: `Error while adding car. Error: ${error.message}`,
+    };
+  }
+}
+
+module.exports = { getCars, changeStatus, deleteCar, addCar };
